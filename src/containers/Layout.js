e655c4a98de686616a2769e349127034599e8fd5
neerajs6 <neeraj_sudhakar@yahoo.com>
2021-09-08 08:08:17 -0400

    first revamp push

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -79,8 +79,8 @@ class CustomLayout extends Component {
     } 
     console.log('dateArray',dateArray)
 
-    // const url = 'https://bsa-web.herokuapp.com/get_stored_categories';
-    const url = 'http://127.0.0.1:8050/get_stored_categories';
+    const url = 'https://bsa-web.herokuapp.com/get_stored_categories';
+    // const url = 'http://127.0.0.1:8050/get_stored_categories';
     const response = await fetch(url , {
     method: 'GET',
     headers: {
@@ -149,8 +149,8 @@ class CustomLayout extends Component {
       recent:'true',
       date:''
     }
-    //  const url = 'https://bsa-web.herokuapp.com/get_articles';
-    const url = 'http://127.0.0.1:8050/get_articles';
+     const url = 'https://bsa-web.herokuapp.com/get_articles';
+    // const url = 'http://127.0.0.1:8050/get_articles';
     const response = await fetch(url , {
     method: 'POST',
     headers: {
@@ -202,8 +202,8 @@ class CustomLayout extends Component {
       recent:'false',
       date:this.state.selectedDate
     }
-    // const url = 'https://bsa-web.herokuapp.com/get_articles';
-    const url = 'http://127.0.0.1:8050/get_articles';
+    const url = 'https://bsa-web.herokuapp.com/get_articles';
+    // const url = 'http://127.0.0.1:8050/get_articles';
     const response = await fetch(url , {
     method: 'POST',
     headers: {
@@ -237,8 +237,8 @@ class CustomLayout extends Component {
   search(event){
      var searchQuery = this.state.qState
 
-    //  const url = `https://bsa-web.herokuapp.com/get_search/${searchQuery}`;
-     const url = `http://127.0.0.1:8050/get_search/${searchQuery}`;
+     const url = `https://bsa-web.herokuapp.com/get_search/${searchQuery}`;
+     // const url = `http://127.0.0.1:8050/get_search/${searchQuery}`;
         fetch(url , {
           method: 'GET',
           headers: {
@@ -310,8 +310,8 @@ class CustomLayout extends Component {
 
   async onFinish (values) {
     console.log(values);
-    // const url = 'https://bsa-web.herokuapp.com/store_email';
-    const url = 'http://127.0.0.1:8050/store_email';
+    const url = 'https://bsa-web.herokuapp.com/store_email';
+    // const url = 'http://127.0.0.1:8050/store_email';
     const response = await fetch(url , {
     method: 'POST',
     headers: {
